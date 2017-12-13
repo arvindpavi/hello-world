cb7ca6f9a59aad605e7fcc4d707ea9cf0290eddc
arvindpavi <arvindp.avi@gmail.com>
2017-12-13 23:12:44 +0530

    Added Entry Component to show ng4 comonent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,41 @@ import { UpgradeModule } from '@angular/upgrade/static';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, forwardRef } from '@angular/core';
 
+import { UpgradeAdapter } from '@angular/upgrade'
+
 import { AppComponent } from './app.component';
 
-declare var angular: angular.IAngularStatic;
+import 'angular'
+
+declare var angular:any;
+const adapter = new UpgradeAdapter(forwardRef(() => AppModule));
 
 @NgModule({
-  /*declarations: [
+  declarations: [
     AppComponent
-  ],*/
+  ],
   imports: [
     BrowserModule,
     UpgradeModule
   ],
+  entryComponents: [AppComponent],
   // providers: [],
   // bootstrap: [AppComponent]
 })
 export class AppModule {
 	constructor(private upgrade: UpgradeModule) { }
 	ngDoBootstrap() {
-		this.upgrade.bootstrap(document.body, ['heroApp'], ['CI']);
+		// this.upgrade.bootstrap(document.body, ['heroApp']);
 	}
 }
 
 angular.module('heroApp', [])
 	.controller('MainCtrl', function() {
 	this.message = 'Hybrid App Works!!!';
-});
+}).directive('helloWorld', function() {
+  return {
+      restrict: 'AE',
+      replace: 'true',
+      template: '<h3>Hello World!!</h3>'
+  };
+}).directive('appRoot', adapter.downgradeNg2Component(AppComponent) as angular.IDirectiveFactory)
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { enableProdMode } from '@angular/core';
+import { UpgradeModule } from '@angular/upgrade/static';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +9,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.documentElement, ['heroApp'], { strictDi: true });
+});
